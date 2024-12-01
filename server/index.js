37257dfa653846f356f58e85e452e9827002dade
Cioaca Stefan <stefanoctavian85@gmail.com>
2024-12-01 21:56:39 +0200

    Changed login endpoint

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'
-import { getAccountByUsername, migrate } from './db/service.js';
+import cors from 'cors';
+import { expressjwt } from 'express-jwt';
+import { getAccountByUsername, getAccountById, migrate } from './db/service.js';
 
 dotenv.config();
 const app = express();
@@ -10,12 +11,21 @@ app.use(cors());
 
 migrate();
 
+// app.use(expressjwt({
+//     secret: 'my-secret',
+//     algorithms: ['HS256'],
+// }));
+
 const PORT = process.env.PORT;
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     const account = await getAccountByUsername(req.body.username);
+
+    if (!account) {
+        return res.status(400).json({message: 'Invalid username or password!' });
+    }
 
     if (username === account.username && password === account.password) {
         return res.status(200).json({ message: 'Login successful!' });
