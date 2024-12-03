0811ba75266223507db99fcdd1edfa1d11fc7225
ciobanumatei03 <ciobanu.matei03@gmail.com>
2024-12-03 10:34:25 +0200

    test

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { expressjwt } from 'express-jwt';
-import { getAccountByUsername, getAccountById, migrate } from './db/service.js';
-import swaggerUi from 'swagger-ui-express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { expressjwt } from "express-jwt";
+import { getAccountByUsername, getAccountById, migrate } from "./db/service.js";
+import swaggerUi from "swagger-ui-express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+//test
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -20,32 +20,36 @@ const __dirname = dirname(__filename);
 
 migrate();
 
-app.use('/api-doc.yml', express.static(path.join(__dirname, 'api-doc.yml')));
+app.use("/api-doc.yml", express.static(path.join(__dirname, "api-doc.yml")));
 
-app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(null, {
+app.use(
+  "/api-doc",
+  swaggerUi.serve,
+  swaggerUi.setup(null, {
     swaggerOptions: { url: `http://localhost:${PORT}/api-doc.yml` },
-}));
+  })
+);
 
-app.get('/api-doc.yml', (req, res) => {
-    res.json(app.apiDoc);
+app.get("/api-doc.yml", (req, res) => {
+  res.json(app.apiDoc);
 });
 
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    const account = await getAccountByUsername(req.body.username);
+  const account = await getAccountByUsername(req.body.username);
 
-    if (!account) {
-        return res.status(400).json({message: 'Invalid username or password!' });
-    }
+  if (!account) {
+    return res.status(400).json({ message: "Invalid username or password!" });
+  }
 
-    if (username === account.username && password === account.password) {
-        return res.status(200).json({ message: 'Login successful!' });
-    }
+  if (username === account.username && password === account.password) {
+    return res.status(200).json({ message: "Login successful!" });
+  }
 
-    return res.status(400).json({ message: 'Invalid username or password!' });
+  return res.status(400).json({ message: "Invalid username or password!" });
 });
 
 app.listen(PORT, () => {
-    console.log(`has started on port ${PORT}!`);
+  console.log(`has started on port ${PORT}!`);
 });
